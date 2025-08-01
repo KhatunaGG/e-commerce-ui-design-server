7bf94ad71b0b734be87d0ed29a1ed1272e17b388
Gagloshvili <KGagloshvili@oriflame.com>
2025-08-01 11:52:21 +0400

    added: address res

---
+++ src/address/address.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { CreateAddressDto } from './dto/create-address.dto';
+import { UpdateAddressDto } from './dto/update-address.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Address } from './schema/address.schema';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class AddressService {
+  constructor(@InjectModel(Address.name) private addressService: Model<Address>){}
+  create(createAddressDto: CreateAddressDto) {
+    return 'This action adds a new address';
+  }
+
+  findAll() {
+    return `This action returns all address`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} address`;
+  }
+
+  update(id: number, updateAddressDto: UpdateAddressDto) {
+    return `This action updates a #${id} address`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} address`;
+  }
+}

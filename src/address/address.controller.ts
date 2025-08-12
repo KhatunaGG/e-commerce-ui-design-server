63639e94817a20b9f68ff6cef1213520e3381523
Gagloshvili <KGagloshvili@oriflame.com>
2025-08-13 00:44:44 +0400

    added: address create logic

--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -1,35 +1,59 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { AddressService } from './address.service';
 import { CreateAddressDto } from './dto/create-address.dto';
 import { UpdateAddressDto } from './dto/update-address.dto';
+import { AuthGuard } from 'src/auth/guard/auth.guard';
+import { Types } from 'mongoose';
+import { Role } from 'src/common/enums/role.enum';
 
 @Controller('address')
 export class AddressController {
   constructor(private readonly addressService: AddressService) {}
 
-  @Post("create-address")
-  create(@Body() createAddressDto: CreateAddressDto) {
-    console.log(createAddressDto, "createAddressDto")
-    return this.addressService.create(createAddressDto);
+  @Post('create-address')
+  @UseGuards(AuthGuard)
+  create(@Req() req, @Body() createAddressDto: CreateAddressDto) {
+    console.log(createAddressDto, 'createAddressDto');
+    return this.addressService.create(req.userId, createAddressDto);
   }
 
   @Get()
-  findAll() {
-    return this.addressService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.addressService.findOne(+id);
+  @UseGuards(AuthGuard)
+  findAll(@Req() req) {
+    const isAdmin = req.role === Role.ADMIN;
+    return this.addressService.findAll(req.userId, isAdmin);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAddressDto: UpdateAddressDto) {
-    return this.addressService.update(+id, updateAddressDto);
+  @UseGuards(AuthGuard)
+  update(
+    @Req() req,
+    @Param('id') id: string,
+    @Body() updateAddressDto: UpdateAddressDto,
+  ) {
+        console.log(req.userId, "userId")
+    console.log(id, "id")
+    console.log(updateAddressDto, "updateAddressDto")
+    return this.addressService.update(req.userId, id, updateAddressDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.addressService.remove(+id);
-  }
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.addressService.findOne(+id);
+  // }
+
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.addressService.remove(+id);
+  // }
 }
---
+++ src/purchase/dto/query-params.dto.ts
@@ -0,0 +1,20 @@
+import { Transform } from 'class-transformer';
+import { IsNumber } from 'class-validator';
+
+export class QueryParamsDto {
+  // @Transform(({ value }) => Number(value))
+  // @IsNumber()
+  // page: number = 1;
+
+  // @Transform(({ value }) => Number(value))
+  // @IsNumber()
+  // take: number = 5;
+
+  @Transform(({ value }) => Number(value) || 1)
+  @IsNumber()
+  page: number = 1;
+
+  @Transform(({ value }) => Number(value) || 5)
+  @IsNumber()
+  take: number = 5;
+}
